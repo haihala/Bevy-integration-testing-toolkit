bd008b811bc64455d18eb3a10abdd01c8326d1a0
Eero Häihälä <eero.haihala@gmail.com>
2024-01-25 17:54:48 +0200

    Add a plugin for making asserts or timing out

--- crates/bitt/src/asserts.rs
+++ crates/bitt/src/asserts.rs
@@ -1,4 +1,6 @@
-use bevy::prelude::*;
+use std::time::Duration;
+
+use bevy::{app::AppExit, prelude::*};
 
 /// A resource that can be used to state the outcome of this test.
 #[derive(Resource, Debug, Default)]
@@ -30,5 +32,39 @@ pub struct AsserterPlugin;
 impl Plugin for AsserterPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<Asserter>();
+    }
+}
+
+#[derive(Debug, Resource)]
+struct Timeout(Duration);
+
+/// A plugin that will add an Asserter and panic if it runs for longer than the given duration.
+///
+/// Useful for cases when you want to test a combo of some systems in relative isolation.
+#[derive(Debug)]
+pub struct TimeoutAsserterPlugin(pub Duration);
+
+impl Plugin for TimeoutAsserterPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<Asserter>()
+            .insert_resource(Timeout(self.0))
+            .add_systems(Update, (timeout, exit_on_asserter_result).chain());
+    }
+}
+
+fn timeout(mut asserter: ResMut<Asserter>, time: Res<Time<Real>>, timeout: Res<Timeout>) {
+    if time.elapsed() >= timeout.0 {
+        dbg!("Test timed out");
+        asserter.fail();
+    }
+}
+
+fn exit_on_asserter_result(asserter: Res<Asserter>, mut exit: EventWriter<AppExit>) {
+    if let Some(result) = asserter.outcome {
+        if result {
+            exit.send(AppExit);
+        } else {
+            panic!("Test failed");
+        }
     }
 }
--- crates/bitt/src/lib.rs
+++ crates/bitt/src/lib.rs
@@ -2,6 +2,6 @@ mod asserts;
 mod headless_default_plugins;
 mod input_playback;
 
-pub use asserts::Asserter;
+pub use asserts::{Asserter, TimeoutAsserterPlugin};
 pub use headless_default_plugins::HeadlessDefaultPlugins;
 pub use input_playback::{PlaybackTestGear, PlaybackTestingOptions};
