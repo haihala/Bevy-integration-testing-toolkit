1029c213ff297198f61ec958c4afca76fd263078
Eero Häihälä <eero.haihala@gmail.com>
2024-02-10 19:01:09 +0200

    Add click demo game

---
+++ crates/click_demo/src/lib.rs
@@ -0,0 +1,162 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::PrimaryWindow};
+
+pub struct ClickDemoPlugin;
+
+#[derive(Debug, Resource)]
+pub struct Points(pub u32);
+
+#[derive(Debug, Component)]
+pub struct Clickable;
+#[derive(Debug, Component)]
+pub struct TimeText;
+#[derive(Debug, Component)]
+pub struct ScoreText;
+
+impl Plugin for ClickDemoPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup).add_systems(
+            Update,
+            (register_clicks, update_score, move_target, handle_timer).chain(),
+        );
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.insert_resource(Points(0));
+
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.0).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            ..default()
+        },
+        Clickable,
+    ));
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|cb| {
+            cb.spawn((
+                TextBundle {
+                    text: Text::from_sections(vec![
+                        TextSection {
+                            value: "Score: ".to_string(),
+                            style: TextStyle {
+                                font_size: 40.0,
+                                color: Color::WHITE,
+                                ..default()
+                            },
+                        },
+                        TextSection {
+                            value: "0".to_string(),
+                            style: TextStyle {
+                                font_size: 40.0,
+                                color: Color::WHITE,
+                                ..default()
+                            },
+                        },
+                    ]),
+                    ..default()
+                },
+                ScoreText,
+            ));
+            cb.spawn((
+                TextBundle {
+                    text: Text::from_sections(vec![
+                        TextSection {
+                            value: "Time: ".to_string(),
+                            style: TextStyle {
+                                font_size: 40.0,
+                                color: Color::WHITE,
+                                ..default()
+                            },
+                        },
+                        TextSection {
+                            value: "60".to_string(),
+                            style: TextStyle {
+                                font_size: 40.0,
+                                color: Color::WHITE,
+                                ..default()
+                            },
+                        },
+                    ]),
+                    ..default()
+                },
+                TimeText,
+            ));
+        });
+}
+
+fn register_clicks(
+    buttons: Res<Input<MouseButton>>,
+    mut points: ResMut<Points>,
+    clickable_query: Query<&Transform, With<Clickable>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform), With<Camera2d>>,
+) {
+    if !buttons.just_pressed(MouseButton::Left) {
+        return;
+    }
+
+    let Ok(clickable_tf) = clickable_query.get_single() else {
+        // This happens when the game is over.
+        return;
+    };
+
+    let (cam, cam_tf) = camera_query.single();
+    let window = window_query.single();
+
+    if let Some(world_pos) = window
+        .cursor_position()
+        .and_then(|cursor| cam.viewport_to_world(cam_tf, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        if (world_pos - clickable_tf.translation.truncate()).length() < 50.0 {
+            points.0 += 1;
+        } else if points.0 > 0 {
+            points.0 -= 1;
+        }
+    }
+}
+
+fn update_score(points: Res<Points>, mut score_text_query: Query<&mut Text, With<ScoreText>>) {
+    if points.is_changed() {
+        score_text_query.single_mut().sections[1].value = points.0.to_string();
+    }
+}
+
+fn move_target(points: Res<Points>, mut clickable_query: Query<&mut Transform, With<Clickable>>) {
+    if points.is_changed() {
+        let angle = points.0 as f32;
+        clickable_query.single_mut().translation = 300.0 * Vec3::new(angle.cos(), angle.sin(), 0.0);
+    }
+}
+
+fn handle_timer(
+    mut time_text_query: Query<&mut Text, With<TimeText>>,
+    time: Res<Time>,
+    mut commands: Commands,
+    clickable_query: Query<Entity, With<Clickable>>,
+) {
+    time_text_query.single_mut().sections[1].value = if time.elapsed_seconds() > 60.0 {
+        for entity in clickable_query.iter() {
+            // This prevents further clicks upon time over
+            commands.entity(entity).remove::<Clickable>();
+        }
+
+        "Over!".to_string()
+    } else {
+        (60.0 - time.elapsed_seconds()).ceil().to_string()
+    };
+}
---
+++ crates/click_demo/src/main.rs
@@ -0,0 +1,78 @@
+use bevy::prelude::*;
+use bitt::{Asserter, HeadlessDefaultPlugins, PlaybackTestGear, PlaybackTestingOptions};
+use clap::{Parser, ValueEnum};
+use click_demo::{ClickDemoPlugin, Points};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+enum IntegrationTestScript {
+    ThreeClicks,
+}
+impl IntegrationTestScript {
+    fn to_string(&self) -> String {
+        match self {
+            IntegrationTestScript::ThreeClicks => "three-clicks",
+        }
+        .to_string()
+    }
+}
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the test script
+    script: Option<IntegrationTestScript>,
+
+    /// Run in headless or not
+    #[arg(long)]
+    headless: bool,
+
+    /// Fails if a script is not recorded
+    #[arg(long)]
+    ci: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut app = App::new();
+
+    if args.headless {
+        app.add_plugins(HeadlessDefaultPlugins);
+    } else {
+        app.add_plugins(DefaultPlugins);
+    }
+
+    app.add_plugins(ClickDemoPlugin);
+
+    match args.script {
+        Some(script) => {
+            app.add_plugins(PlaybackTestGear::new(
+                script.to_string(),
+                PlaybackTestingOptions {
+                    read_only: args.ci,
+                    ..default()
+                },
+            ));
+
+            match script {
+                IntegrationTestScript::ThreeClicks => {
+                    app.add_systems(Update, assert_score_of_three);
+                }
+            }
+        }
+        _ => {
+            assert!(!args.headless, "A script must be provided in headless mode");
+            assert!(!args.ci, "A script must be provided in CI mode");
+        }
+    }
+
+    app.run();
+}
+
+fn assert_score_of_three(score: Res<Points>, mut asserter: ResMut<Asserter>) {
+    if score.0 == 3 {
+        asserter.pass();
+    }
+}
+
+// Add one test in headless mode that uses a faster time scale
